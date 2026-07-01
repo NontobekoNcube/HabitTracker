1409fdbb2abb5cf118848d73e76505bff9a82c1f
NontobekoNcube <nontoncube7@gmail.com>
2026-07-01 20:38:24 +0200

    added tests

--- test_habits.py
+++ test_habits.py
@@ -30,5 +30,26 @@ def test_is_broken_with_broken_streak():
 
 
 # test functions for longest_streak:empty completion_dates list, one date, consecutive dates with no break, dates with a break:
-def test_longest_streak():
+def test_longest_streak_empty():
+    #when completion_dates is empty: should return 0
+    habit = Habit("coding", "daily", 14)
+    assert habit.get_longest_streak() == 0
+
+def test_longest_streak_one_date():
+    #completion_dates has one date
+    habit = Habit("coding", "daily", 14)
+    habit.completion_dates = [date.today()]
+    assert habit.get_longest_streak() == 1
+
+#def test_longest_streak_consecutive_dates_with_no_break():
+    #completion_dates has consecutive dates with no break
+    habit =  Habit("coding", "daily", 14)
+    habit.completion_dates = [date.today()- timedelta(days=2), date.today() - timedelta(days=1), date.today()]
+    assert habit.get_longest_streak() == 3
+
+#def test_longest_streak_dates_with_a_break():
+    habit =  Habit("coding", "daily", 14)
+    habit.completion_dates = [date.today()- timedelta(days=2), date.today() - timedelta(days=1)]
+    assert habit.get_longest_streak() == 2
+
     
