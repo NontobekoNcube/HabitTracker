dd73d80d4a02922704c9170e803ea751b1a611a4
NontobekoNcube <nontoncube7@gmail.com>
2026-07-09 11:30:04 +0200

    add create table statements to db.py

--- habit_tracker/db.py
+++ habit_tracker/db.py
@@ -1,2 +1,33 @@
 import sqlite3
 from datetime import date
+
+def get_connection():
+    conn = sqlite3.connect("habits.db") #connects to habits.db file
+    return conn
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXITS habits(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NOT NULL,
+                   periodicity TEXT,
+                   target_period INTEGER,
+                   creationdate TEXT)
+                   """)
+    
+    cursor.execute("""
+                   CREATE  TABLE IF NOT EXISTS completions(
+                   completion_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   habit_id INTEGER NOT NULL,
+                   date TEXT NOT NULL,
+                   FOREIGN KEY (habit_id) REFERENCES habits(id)
+                   )
+                   """)
+    
+    conn.commit()
+    conn.close()
+    
+        
